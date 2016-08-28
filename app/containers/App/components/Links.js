3f713adc8479a7472f65dfcc24c22390c5940b1b
nealfennimore <hi@neal.codes>
2016-08-28 12:54:25 -0700

    Fix links alignment

--- app/containers/App/components/Links.js
+++ app/containers/App/components/Links.js
@@ -4,14 +4,16 @@ import styles from './Links.css';
 
 const MyComponent = (props) => {
     return (
-        <ul className={`row ${styles.links}`}>
-            <li className={`column shrink ${styles.link}`}>
-                <Link to="/" activeClassName={styles.active}>Transactions</Link>
-            </li>
-            <li className={`column shrink ${styles.link}`}>
-                <Link to="/forecast" activeClassName={styles.active}>Forecast</Link>
-            </li>
-        </ul>
+        <div className={`column small-12`}>
+            <ul className={`row ${styles.links}`}>
+                <li className={`column shrink ${styles.link}`}>
+                    <Link to="/" activeClassName={styles.active}>Transactions</Link>
+                </li>
+                <li className={`column shrink ${styles.link}`}>
+                    <Link to="/forecast" activeClassName={styles.active}>Forecast</Link>
+                </li>
+            </ul>
+        </div>
     );
 };
 
